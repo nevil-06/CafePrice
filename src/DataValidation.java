304ff9c6a415d9516b28b90dd11264f23d8ff410
Arnab Sinha <sarnab433@gmail.com>
2023-10-02 16:44:31 -0400

    Added code to test text split to remove non-alphanumeric characters from text

--- src/DataValidation.java
+++ src/DataValidation.java
@@ -1,2 +1,22 @@
+import java.util.*;
+
 public class DataValidation {
+
+    public static final String delimiter_split_list = "[[ ]*|[,]*|[)]*|[(]*|[\"]*|[;]*|[-]*|[:]*|[']*|[\\.]*|[:]*|[/]*|[!]*|[?]*|[+]*]+";
+    public static String[] regexValidate(String text_to_validate){
+
+        String[] text_split = text_to_validate.split(delimiter_split_list);
+
+        return text_split;
+    }
+
+    public static void main(String[] args){
+
+        String test = "Arnab()()Prerna;';'HEHE;;";
+        String[] res = regexValidate(test);
+
+        for(String s: res){
+            System.out.println(s);
+        }
+    }
 }
